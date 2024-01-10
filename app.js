3be6c5c67577c485d3768ab8ff8e91e0701dd876
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2024-01-10 17:05:06 +0600

    info saved through https request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const mailchimp = require("@mailchimp/mailchimp_marketing");  
+const https = require("https");
 
 const listId = "565ca59cd4";
 const apiKey = "a16d3b06878ab0524bae9d224f5b6df6-us21";
@@ -21,36 +22,48 @@ app.get("/", function(req,res){
 app.post("/", function(req,res){
   const fName = req.body.fName;
   const lName = req.body.lName;
-  const email = req.body.email
+  const email = req.body.email;
 
-  mailchimp.setConfig({
-    apiKey: apiKey,
-    server: "us21",
-  });
-
-  const run = async () => {
-    const response = await mailchimp.lists.addListMember(listId,
+  const data = {
+    members: [
         {
             email_address: email,
             status: "subscribed",
             merge_fields: {
-                FNAME: fName,
-                LNAME: lName,
-                
-            },
-        },
-        {
-            skipMergeValidation: false
+                FNAME : fName,
+                LNAME : lName
+            }
         }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
+  console.log(jsonData);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/565ca59cd4";
+    const apiKey = "850da8aac4c5ea34e034c5fef103190b-us21";
+    const options = {
+        method: "POST",
+        headers: {
+            Authorization: "Basic " + Buffer.from("shariarsakil:" + apiKey).toString("base64"),
+          },
+      };
+
+    const request = https.request(url, options, function(response){
+      
+          var data2 = "";
+          response.on("data", function(chunk){
+            data2 += chunk;
+          })
+
+          
+          response.on("end", function(){
+            console.log(JSON.parse(data2));
+          })
+
         
-    );
-};
-
-run();
-
-res.send(res.statusCode);
-
-
+    })
+    request.write(jsonData);
+    request.end();
 
 
 })
