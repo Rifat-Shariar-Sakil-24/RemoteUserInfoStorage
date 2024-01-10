abd3c8c3f9f2f4d48a39f20c1a4f3b4bab8495a9
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2024-01-10 15:41:53 +0600

    console.log(req.body);

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/signup.html");
 })
-
+app.post("/", function(req,res){
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const email = req.body.email
+  console.log(fName,lName,email);
+})
 app.listen(4000, function(){
     console.log("server is running on port 4000");
 })
